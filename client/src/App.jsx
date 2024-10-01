6fff41740ed088e72f217ad492a7d8c3a546a22f
HossainRafi <hossainrafi71@gmail.com>
2024-10-01 16:34:19 +0600

    routing setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,10 @@
+import { Outlet } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <h1>hello world</h1>
+      <Outlet />
     </>
   );
 }
--- client/src/routes/router.jsx
+++ client/src/routes/router.jsx
@@ -1,9 +1,21 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "./../App";
+import Home from "../pages/home/Home";
+import ShopPage from "../pages/shop/ShopPage";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/shop",
+        element: <ShopPage />,
+      },
+    ],
   },
 ]);
