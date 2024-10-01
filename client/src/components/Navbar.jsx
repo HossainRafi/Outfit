0777cb3cd73b52714962c061353b3afd15b415b2
HossainRafi <hossainrafi71@gmail.com>
2024-10-01 16:56:08 +0600

    search icon

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,11 +1,11 @@
 import { Link, NavLink } from "react-router-dom";
-import Logo from "../../src/assets/logo.png"
+import Logo from "../../src/assets/logo.png";
+import { IoIosSearch } from "react-icons/io";
 
 const Navbar = () => {
   return (
     <header className="fixed-nav-bar w-nav">
       <nav className="max-w-screen-2xl mx-auto px-4 flex justify-between items-center bg-blue-500">
-
         {/* nav links items */}
         <ul className="nav__links">
           <li className="link">
@@ -41,11 +41,21 @@ const Navbar = () => {
             </NavLink>
           </li>
         </ul>
+
         {/* logo */}
         <div className="nav__logo">
           <Link to="/">
             <img src={Logo} />
           </Link>
+        </div>
+
+        <div className="nav__icons relative">
+          {/* search icon */}
+          <span>
+            <Link to="/search">
+              <IoIosSearch className="text-2xl font-bold" />
+            </Link>
+          </span>
         </div>
       </nav>
     </header>
