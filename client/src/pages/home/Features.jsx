665826a7a9d13e2f2c511ded3dafa8bcfd652160
HossainRafi <hossainrafi71@gmail.com>
2024-10-03 18:28:02 +0600

    change

--- client/src/pages/home/Features.jsx
+++ client/src/pages/home/Features.jsx
@@ -5,46 +5,52 @@ import { AiFillDollarCircle } from "react-icons/ai";
 
 const Features = () => {
   return (
-    <section className="section__container banner__container">
-      <div className="banner__card">
-        <span>
-          <FaTruck />
-        </span>
-        <h4>Free Delivery</h4>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit eius soluta
-        </p>
-      </div>
+    <section className="section__container">
+      <div className="banner__container">
+        {/* free delivery div */}
+        <div className="banner__card">
+          <span>
+            <FaTruck />
+          </span>
+          <h4>Free Delivery</h4>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit eius soluta
+          </p>
+        </div>
 
-      <div className="banner__card">
-        <span>
-          <i className="ri-money-dollar-circle-line"></i>
-          <AiFillDollarCircle />
-        </span>
-        <h4>Money Back Guaranty</h4>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit eius soluta
-        </p>
-      </div>
+        {/* money back div */}
+        <div className="banner__card">
+          <span>
+            <i className="ri-money-dollar-circle-line"></i>
+            <AiFillDollarCircle />
+          </span>
+          <h4>Money Back Guaranty</h4>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit eius soluta
+          </p>
+        </div>
 
-      <div className="banner__card">
-        <span>
-          <ImHappy2 />
-        </span>
-        <h4>Satisfaction Guaranty</h4>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit eius soluta
-        </p>
-      </div>
+        {/* happy client div */}
+        <div className="banner__card">
+          <span>
+            <ImHappy2 />
+          </span>
+          <h4>Satisfaction Guaranty</h4>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit eius soluta
+          </p>
+        </div>
 
-      <div className="banner__card">
-        <span>
-          <RiUserVoiceFill />
-        </span>
-        <h4>Strong Support</h4>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit eius soluta
-        </p>
+        {/* support div */}
+        <div className="banner__card">
+          <span>
+            <RiUserVoiceFill />
+          </span>
+          <h4>Strong Support</h4>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit eius soluta
+          </p>
+        </div>
       </div>
     </section>
   );
