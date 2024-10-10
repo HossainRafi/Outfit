f28a0a3a39695c845e81bda114d90fb515f12492
HossainRafi <hossainrafi71@gmail.com>
2024-10-10 19:24:46 +0600

    connection of mongodb

--- server/index.js
+++ server/index.js
@@ -1,6 +1,21 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 const port = process.env.PORT || 5000;
+
+// hossainrafi71;
+// SkZurglfGOv7oUeP;
+
+async function main() {
+  await mongoose.connect(
+    "mongodb+srv://hossainrafi71:SkZurglfGOv7oUeP@outfit.at2dm.mongodb.net/?retryWrites=true&w=majority&appName=Outfit"
+  );
+}
+main()
+  .then(() =>
+    console.log("MongoDB Connected Successfully=================================== !!!!")
+  )
+  .catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
   res.send("Outfit Websites Server is Running");
