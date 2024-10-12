4235f8c96e50bed68084e97e45aa1a3c07c5c2b6
HossainRafi <hossainrafi71@gmail.com>
2024-10-12 20:36:49 +0600

    user route and cors

--- server/index.js
+++ server/index.js
@@ -3,9 +3,27 @@ const app = express();
 require("dotenv").config();
 const mongoose = require("mongoose");
 const port = process.env.PORT || 5000;
+const cors = require("cors");
+
+// middleware
+app.use(express.json());
+app.use(
+  cors({
+    origin: "https://localhost:5173",
+    credentials: true,
+  })
+);
+
+// routes
+const userRoutes = require("./src/users/user.route");
+
+app.use("/api/auth", userRoutes);
 
 async function main() {
   await mongoose.connect(process.env.DB_URL);
+  app.get("/", (req, res) => {
+    res.send("Outfit Websites Server is Running");
+  });
 }
 main()
   .then(() =>
@@ -14,10 +32,6 @@ main()
     )
   )
   .catch((err) => console.log(err));
-
-app.get("/", (req, res) => {
-  res.send("Outfit Websites Server is Running");
-});
 
 app.listen(port, () => {
   console.log(
