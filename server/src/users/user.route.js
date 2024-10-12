b64fa5d0377078843763a6e910453763ceb4ea0f
HossainRafi <hossainrafi71@gmail.com>
2024-10-12 20:44:13 +0600

    testing user router

--- server/src/users/user.route.js
+++ server/src/users/user.route.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-
-
-
-
-
-
+router.get("/", async (req, resizeBy) => {
+  try {
+    res.send("Outfit Websites Server is Running");
+  } catch (error) {
+    console.error("Error: ", error);
+  }
+});
 
 module.exports = router;
